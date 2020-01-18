e25f9ddb110501ae66c3756bc587b34bc762e26f
weidingergithub <58649599+weidingergithub@users.noreply.github.com>
2020-01-17 20:26:28 -0500

    j

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const app = express();
+const app = express(); 
 
 app.get('/', function (req, res) {
-  res.send('Hello ECS automated');
+  res.send('Hello ECS automated zzzzzz');
 });
 
 app.listen(3000, function () {
